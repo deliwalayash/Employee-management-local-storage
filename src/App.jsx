c7b1c33e71d442ea8a5943ddce67cbb92596ca8d
deliwayash <yashdeliwala10@gmail.com>
2025-12-04 12:28:23 +0530

    resolved cherry pick conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,40 @@ import Controls from './components/Controls'
 function App() {
   const [employee,setEmployee]=useState(JSON.parse(localStorage.getItem("employee") )|| [])
   const [editemp,seteditemp]=useState(null)
+  const [search,setSearch]=useState("")
+  const [sortOrder,setSortorder]=useState("")
+  const [searchDepartment,setSearchDepartment]=useState("")
+
+
+
+    const foundEmployee=employee.filter((curEle)=>{
+        return curEle.name.toLowerCase().includes(search.toLowerCase())
+    })
+
+    let finalEmloyess=[...foundEmployee]
+
+    if(sortOrder == "asc"){
+      finalEmloyess.sort((a,b)=>{return b.salary-a.salary})
+    }
+    else if(sortOrder == "des"){
+        finalEmloyess.sort((a,b)=>{return a.salary -b.salary})
+    }
+
+   if(searchDepartment !== ""){
+     finalEmloyess=finalEmloyess.filter((curEle)=>{
+        return curEle.department == searchDepartment
+    })
+   }
+
+   if (searchDepartment !== "" && finalEmloyess.length === 0) {
+  Swal.fire("Oops!", "No members found in this department", "info");
+}
+
+
+  
+
+
+
 
   const deleteemployee=(id)=>{
     const updateemployess=employee.filter((curEle)=>{
@@ -18,8 +52,36 @@ function App() {
     })
     
 
+<<<<<<< HEAD
     setEmployee(updateemployess)
   }
+=======
+  Swal.fire({
+    title: "Are you sure?",
+    text: "This action cannot be undone!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!"
+  }).then((result) => {
+
+    if (result.isConfirmed) {
+      // your delete logic here
+      const updated = employee.filter((curEle) => curEle.id !== id);
+      setEmployee(updated);
+
+
+      Swal.fire("Deleted!", "Employee has been removed.", "success");
+    }
+
+  });
+
+};
+
+
+  
+>>>>>>> 3f00a08 (seacr sorting and filtering operation done)
    const editemployee = (id)=>{
     const findemp= employee.find((curEle)=>{
       return curEle.id == id
@@ -31,8 +93,17 @@ function App() {
   },[employee])
   return (
     <>
+<<<<<<< HEAD
   <Form2 setEmployee={setEmployee} editemp={editemp} seteditemp={seteditemp}></Form2>
     <Displaytable employee={employee} deleteemployee={deleteemployee} editemployee={editemployee}></Displaytable>
+=======
+    
+
+  <Form2 setEmployee={setEmployee} editemp={editemp} seteditemp={seteditemp} updataEmployee={updataEmployee}></Form2>
+  <Controls setSearch={setSearch} setSortorder={setSortorder} setSearchDepartment={setSearchDepartment}></Controls>
+    <Displaytable employee={finalEmloyess} deleteemployee={deleteemployee} editemployee={editemployee} ></Displaytable>
+      <ToastContainer position="top-right" />
+>>>>>>> 3f00a08 (seacr sorting and filtering operation done)
     </>
   )
 }
